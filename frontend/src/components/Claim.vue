bb2ecd92b1337ffb06d9b8982b7b366c6a4ac482
syntrust <lundeng@quarkchain.org>
2024-01-02 14:59:08 +0800

    links update

--- frontend/src/components/Claim.vue
+++ frontend/src/components/Claim.vue
@@ -135,7 +135,7 @@ export default {
       this.isImageModalActive = true;
     },
     tokenUrl(id) {
-      return `https://web3q.io/${this.chainConfig.nft}:w3q-g/compose/string!${id}.svg`;
+      return `https://${this.chainConfig.nft}.w3q-g.w3link.io/compose/string!${id}.svg`;
     },
   },
   computed: {
@@ -169,7 +169,7 @@ export default {
       return arr;
     },
     metaLink() {
-      return `https://web3q.io/0x31bc4dAd21fAd6212082C7953379bb62187ffE94:w3q-g/2/0.png`;
+      return `https://0x31bc4dAd21fAd6212082C7953379bb62187ffE94.w3q-g.w3link.io/2/0.png`;
     },
   },
   watch: {
